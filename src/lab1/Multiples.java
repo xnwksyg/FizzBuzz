ef2f4cb980af39611679a2243da0efa8ebc8f0cf
邓涵予 <denghanyu@MacBook-Air-603.local>
2025-09-12 16:16:15 -0400

    Multiples-multiples method overloading task

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -16,4 +16,16 @@ public class Multiples {
         }
         return count;
     }
+
+    public static int multiples() {
+        int count = 0;
+        for (int i = 1; i < 1000; i++) {
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
+
+            if (divisibleBy3 || divisibleBy5){
+                count++;}
+        }
+        return count;
+    }
 }
--- test_files/readme.md
+++
@@ -1,4 +0,0 @@
-# Testing
-
-> Once you complete the later tasks, you can copy over these files
-> to `test/lab1` to test your code.
