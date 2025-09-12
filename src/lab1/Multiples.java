be65d4963e766b5596172e572616ee21bf8b6035
邓涵予 <denghanyu@MacBook-Air-603.local>
2025-09-12 15:53:09 -0400

    Multiples task

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -1,4 +1,19 @@
 package lab1;
 
 public class Multiples {
+    public static void main(String[] args) {
+        System.out.println(multiples(1000, 3,5));
+    }
+
+    static int multiples(int n, int a, int b) {
+        int count = 0;
+        for (int i = 1; i < n; i++) {
+            boolean divisibleBy3 = i % a == 0;
+            boolean divisibleBy5 = i % b == 0;
+
+            if (divisibleBy3 || divisibleBy5){
+                count++;}
+        }
+        return count;
+    }
 }
